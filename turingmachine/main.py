e4bc86ce4ced1ea61bbaac1aa0f678cb0c4aadf0
Miguel Aurelio Oxte Tzuc <60526535+MiguelOxte03@users.noreply.github.com>
2021-01-26 18:14:39 -0500

    Cambios después de la revisión del profe 26/01/2021

--- turingmachine/main.py
+++ turingmachine/main.py
@@ -1,39 +1,31 @@
 from turingmachine import turing_M
 
 print("Maquina de turing 1")
-
-turing_M (state = 'q0', #estado inicial de la maquina de turing
-              blank = 'b', #simbolo blanco de el alfabeto dela cinta
-              tape = list("1"),#inserta los elementos en la cinta
-              final = 'q2',  #estado valido y/o final
-              rules = map(tuple,#reglas de transicion
-                          [
-                          "q0 0 0 right q0".split(),
-                          "q0 1 1 right q0".split(),
-                          "q0 b b left q1".split(),
-                          "q1 1 0 left q1".split(),
-                          "q1 0 1 right q2".split(),
-                          "q1 0 1 left q2".split(),
-                          "q1 b 1 left q2".split(),
-                          ]
-                         )
-             )
-
-print("Maquina de turing 2")
-
-turing_M (state = 'q0',
-              blank = 'b',
-              tape = list("111"),
-              final = 'q2',
-              rules = map(tuple,
-                          [
-                          "q0 0 0 right q0".split(),
-                          "q0 1 1 right q0".split(),
-                          "q0 b b left q1".split(),
-                          "q1 1 0 left q1".split(),
-                          "q1 0 1 right q2".split(),
-                          "q1 0 1 left q2".split(),
-                          "q1 b 1 left q2".split(),
-                          ]
-                         )
-             )
+# Ejercicio Numero 3 del sigueinte enlace
+# https://www.matesfacil.com/automatas-lenguajes/Maquina-Turing.html
+# Entradas (No acepta 1 de entrada inicial)
+# 0101
+# 00110011
+# 0
+# 01010101
+turing_M(state='q0',  # estado inicial
+         blank='b',  # variable por defecto de la cinta
+         tape=list("0101"),  # entradas del automata
+         final='q4',  # estado de aceptacion
+         # reglas de transicion
+         rules=map(tuple,
+                   [
+                       "q0 Y Y right q0".split(),
+                       "q0 b b right q4".split(),
+                       "q0 0 X right q1".split(),
+                       "q1 0 0 right q1".split(),
+                       "q1 Y Y right q3".split(),
+                       "q1 1 Y left q2".split(),
+                       "q3 Y Y right q3".split(),
+                       "q3 1 Y left q2".split(),
+                       "q2 Y Y left q2".split(),
+                       "q2 0 0 left q2".split(),
+                       "q2 X X right q0".split(),
+                   ]
+                   )
+         )
